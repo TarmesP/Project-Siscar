6add5f69d997af2538723df631cdbbdb5cfc49bc
Tales <143357313+TarmesP@users.noreply.github.com>
2024-09-06 09:37:47 -0300

    Add files via upload

---
+++ sol-siscar/ca-siscar-parte3-menuinteraivo-MemoriaPersistenciaFisica/VEICULOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca_siscar_parte1_menuinterativo
+{
+    internal class VEICULOS
+    {
+
+        public int veiid { get; set; }
+
+        public int veianofabricacao { get; set; }
+
+        public int veianomodelo { get; set; }
+
+        public string veinome { get; set; }
+
+        public string veiobservacoes { get; set; }
+
+    }
+}
