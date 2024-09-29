258cf0f835a22fbd738afebd69beb51d961725ac
Tales <143357313+TarmesP@users.noreply.github.com>
2024-09-29 20:33:29 -0300

    Add files via upload

---
+++ sol-siscar/ca-siscar-parte4-menuinteraivo-MemoriaClienteServidor/MODELOS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca_siscar_parte1_menuinterativo
+{
+    internal class MODELOS
+    {
+        public int modid { get; set; }
+
+        public string modnome { get; set; }
+
+        public string modobservacoes { get; set; }
+
+    }
+}
